8f90a56c56df8ad41d7d3479d9973718f0bc9d28
rutvikbangar <rutvikbangar37@gmail.com>
2024-01-11 13:21:38 +0530

    medication page added

---
+++ lib/pages/medication_reminder.dart
@@ -0,0 +1,15 @@
+import 'package:chatme/pages/homepage.dart';
+import 'package:chatme/widgets/widget.dart';
+import 'package:flutter/material.dart';
+
+class MedicationReminder extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(child: Text("Medication"),)
+
+    );
+  }
+
+}
